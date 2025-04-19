5314e07e955c56addc887017566d9799c60dd595
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-04-19 12:32:59 +0200

    fetched and displayed data for single post, improved header so it shows logout if user is logged in

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,23 @@
 import { Link } from "react-router-dom";
-
+import Auth from "./AuthContext";
+import { useContext } from "react";
 export default function Header() {
+
+    const { user, setUser,setToken } = useContext(Auth.Context);
+
+    function handleLogout() {
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+        setUser("");
+        setToken("");
+    }
+
     return (
         <>
             <Link to="/">Home</Link>
-            <Link to="/login">Log in</Link>
+            
+            {user ? <button onClick={handleLogout}>Log out</button> : <Link to="/login">Log in</Link>}
+            
             <Link to="/posts">Posts</Link>
             
         </>
--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,11 +1,31 @@
 import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
 import Comments from "./Comments"
+import Header from "./Header";
 export default function SinglePost() {
-    const {id}= useParams();
+    const { id } = useParams();
+    const [post, setPost] = useState([]);
+
+    function convertDate(date) {
+        return new Date(date).toLocaleDateString("en-GB").split("/").join(".")
+    }
+    useEffect(() => {
+        async function getPost(id) {
+            const res = await fetch(`https://blog-api-rrvr.onrender.com/posts/${id}`);
+            const resData = await res.json();
+            console.log(resData);
+            setPost(resData);
+        }
+        getPost(id);
+    }, [])
     console.log(id);
     return (
         <>
+            <Header/>
             <h1>Single Post</h1>
+            <h1>{post.title}</h1>
+            <p>{post.text}</p>
+            <p>{convertDate(post.createdAt)}</p>
             <Comments postId={id} />
         </>
     )
